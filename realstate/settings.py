4fbaf90af0b54db8b0a725c1f58e573c999ab092
Prabodha Jayawardena <pmrjayawardena@gmail.com>
2020-12-10 11:16:39 +0530

    added inquery check and send email

--- realstate/settings.py
+++ realstate/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# email config
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "princeprabodha03@gmail.com"
+EMAIL_HOST_PASSWORD = "T&Q8OHwi2"
+EMAIL_USE_TLS = True
